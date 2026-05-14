9e5b91792472fd864ff938e0da377739c3324c5c
veritozy <verozy26@gmail.com>
2026-05-13 22:29:06 -0500

    cambios finales

---
+++ src/app/features/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Nav } from "../../shared/nav/nav";
+import { Hero } from "../../shared/hero/hero";
+
+@Component({
+  selector: 'app-home',
+  imports: [Nav, Hero],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
